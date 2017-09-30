45d0023e7c20ebcb230827f4a89c2669fcaee6fd
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-10-01 04:32:44 +0530

    Data commit

--- Players/Combine.py
+++ Players/Combine.py
@@ -2,19 +2,18 @@ import numpy as np
 import cv2
 import os
 
-base = 'Pictures1/'
+base = 'Worked/'
 images = os.listdir(base)
 
-for j in range(0,len(images), 50):
-	output = cv2.imread(base+images[j])
-	image1 = cv2.imread(base+images[j + 1])
-	cv2.addWeighted(image1, 1.0/100, output, 1.0/100, 0, output)
+output = cv2.imread(base+images[0])
+image1 = cv2.imread(base+images[1])
+cv2.addWeighted(image1, 1.0/len(images), output, 1.0/len(images), 0, output)
 
-	for i in range(j + 2,min(j + 100, len(images))):
+for i in range(2,len(images)):
 
-		# load the image
-		image1 = cv2.imread(base+images[i])
-		cv2.addWeighted(image1, 1.0/min(100, len(images) - j), output, 1, 0, output)
-	cv2.imwrite("Pictures2/Output" + str(j) + ".jpg", output)
+	# load the image
+	image1 = cv2.imread(base+images[i])
+	cv2.addWeighted(image1, 1.0/len(images), output, 1, 0, output)
+cv2.imwrite("OutputW.jpg", output)
 
 
