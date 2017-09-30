d80a250cf33f400b571ae09aa981a717bd266028
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-10-01 04:32:23 +0530

    Better combination

---
+++ Combine.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+import os
+
+base = 'Pictures/'
+images = os.listdir(base)
+
+for j in range(0,len(images), 50):
+	output = cv2.imread(base+images[j])
+	image1 = cv2.imread(base+images[j + 1])
+	cv2.addWeighted(image1, 1.0/100, output, 1.0/100, 0, output)
+
+	for i in range(j + 2,min(j + 100, len(images))):
+
+		# load the image
+		image1 = cv2.imread(base+images[i])
+		cv2.addWeighted(image1, 1.0/min(100, len(images) - j), output, 1, 0, output)
+	cv2.imwrite("Pictures2/Output" + str(j) + ".jpg", output)
+
+
